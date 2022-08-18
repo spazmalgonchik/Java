8fc56cce0a57e028095f8710df215b023538305e
spazmalgonchik <spazmalgonchik@gmail.com>
2022-08-18 22:07:45 +0300

    Немного рефакторинга добавил красивого

--- sandbox/src/main/java/ru/prnkstr/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/prnkstr/sandbox/MyFirstProgram.java
@@ -2,23 +2,24 @@ package ru.prnkstr.sandbox;
 
 public class MyFirstProgram {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		int x = 99;
-		int y = 32;
-		int x0 = 888;
-		int y0 = 14;
+        int x = 99;
+        int y = 32;
+        int x0 = 888;
+        int y0 = 14;
 
-		Point p1 = new Point(x0,y0);
-		Point p2 = new Point(x,y);
-		Point points = new Point(x,y,x0,y0);
+        Point p1 = new Point(x0, y0);
+        Point p2 = new Point(x, y);
+        Point points = new Point(x, y, x0, y0);
 
-		System.out.println("Расстояние между двумя точками = " + distance(p1,p2));
-		System.out.println("Расстояние между двумя точками = " + points.distance_test());
+        System.out.println("Расстояние между двумя точками = " + distance(p1, p2));
+        System.out.println("Расстояние между двумя точками = " + points.distance_test());
 
-	}
-	public static double distance(Point p1, Point p2){
-		return Math.sqrt((p2.x - p1.x)*(p2.x - p1.x) + (p2.y - p1.y)*(p2.y - p1.y));
-	}
+    }
+
+    public static double distance(Point p1, Point p2) {
+        return Math.sqrt((p2.x - p1.x) * (p2.x - p1.x) + (p2.y - p1.y) * (p2.y - p1.y));
+    }
 
 }
