a41c66084846d39a27ae8849e59b87b2c6e5fd62
spazmalgonchik <spazmalgonchik@gmail.com>
2022-08-19 14:14:17 +0300

    Update Point.java

--- sandbox/src/main/java/ru/prnkstr/sandbox/Point.java
+++ sandbox/src/main/java/ru/prnkstr/sandbox/Point.java
@@ -6,20 +6,20 @@ public class Point {
     public double x0;
     public double y0;
 
-    public Point (double x,double y){
+    public Point(double x, double y) {
         this.x = x;
         this.y = y;
     }
 
-    public Point (double x,double y, double x0,double y0){
+    public Point(double x, double y, double x0, double y0) {
         this.x = x;
         this.y = y;
         this.x0 = x0;
         this.y0 = y0;
     }
 
-    public double distance_test(){
-        return Math.sqrt((this.x - this.x0)*(this.x - this.x0) + (this.y - this.y0)*(this.y - this.y0));
+    public double distance_test() {
+        return Math.sqrt((this.x - this.x0) * (this.x - this.x0) + (this.y - this.y0) * (this.y - this.y0));
     }
 
 }
