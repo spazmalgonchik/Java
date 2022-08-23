23a1a72ab271be379529aadf7764e374d46d5564
spazmalgonchik <spazmalgonchik@gmail.com>
2022-08-23 16:10:36 +0300

    GroupCreationTests refactor

--- addressbook-web-tests/src/test/java/ru/prnkstr/addressbook/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/prnkstr/addressbook/GroupCreationTests.java
@@ -15,7 +15,7 @@ public class GroupCreationTests {
 
     @BeforeMethod
     public void setUp() {
-        System.setProperty("webdriver.chrome.driver", "E:\\Java\\java_test\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "..\\chromedriver.exe");
         wd = new ChromeDriver();
         wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
     }
@@ -33,14 +33,11 @@ public class GroupCreationTests {
         wd.findElement(By.xpath("//input[@value]")).click();
         wd.findElement(By.linkText("groups")).click();
         wd.findElement(By.name("new")).click();
-        wd.findElement(By.name("group_name")).click();
         wd.findElement(By.name("group_name")).sendKeys("Test_Group_Name_1");
-        wd.findElement(By.name("group_header")).click();
         wd.findElement(By.name("group_header")).sendKeys("Test");
         wd.findElement(By.name("group_header")).sendKeys("Test_Group_Header_1");
         wd.findElement(By.name("group_footer")).sendKeys("Test_Group_Footer_1");
         wd.findElement(By.name("submit")).click();
         wd.findElement(By.linkText("group page")).click();
-        wd.manage().window().setSize(new Dimension(1900, 1020));
     }
 }
