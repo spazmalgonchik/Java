6d1211afd7c2267d597ade56fcbd629c3a97b81e
spazmalgonchik <spazmalgonchik@gmail.com>
2022-08-23 19:37:00 +0300

    added GroupModofication tests

--- addressbook-web-tests/src/test/java/ru/prnkstr/addressbook/appmanager/GroupHelper.java
+++ addressbook-web-tests/src/test/java/ru/prnkstr/addressbook/appmanager/GroupHelper.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
 import ru.prnkstr.addressbook.model.GroupData;
 
-public class GroupHelper extends HelperBase{
+public class GroupHelper extends HelperBase {
 
     public GroupHelper(ChromeDriver wd) {
         super(wd);
@@ -34,5 +34,13 @@ public class GroupHelper extends HelperBase{
 
     public void selectGroup() {
         click(By.name("selected[]"));
+    }
+
+    public void initGroupModification() {
+        click(By.xpath("//input[@value=\"Edit group\"][1]"));
+    }
+
+    public void submitGroupModification() {
+        click(By.name("update"));
     }
 }
--- addressbook-web-tests/src/test/java/ru/prnkstr/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/prnkstr/addressbook/tests/GroupModificationTests.java
@@ -1,4 +1,18 @@
 package ru.prnkstr.addressbook.tests;
 
-public class GroupModificationTests {
+import org.testng.annotations.Test;
+import ru.prnkstr.addressbook.model.GroupData;
+
+public class GroupModificationTests extends TestBase {
+
+    @Test
+    public void testGroupModification() {
+        app.getNavigationHelper().gotoGroupPage();
+        app.getGroupHelper().selectGroup();
+        app.getGroupHelper().initGroupModification();
+        app.getGroupHelper().fillGroupForm(new GroupData("QQQQQ", "WWWWW", "RRRRR"));
+        app.getGroupHelper().submitGroupModification();
+        app.getGroupHelper().returnToGroupPage();
+
+    }
 }
